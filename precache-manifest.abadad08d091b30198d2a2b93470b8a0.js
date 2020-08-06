168300310b4093bb02e7ac70872bc2a951818a0d
Akibarika <lxclxc89816@gmail.com>
2020-08-07 01:18:14 +1200

    Updates

---
+++ precache-manifest.abadad08d091b30198d2a2b93470b8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dae0d93b1961a6cb307e46ce179471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65262c5296a3e8bb9eee",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0db378ef134f8e4c1f7",
+    "url": "/static/js/2.e66c510f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e66c510f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65262c5296a3e8bb9eee",
+    "url": "/static/js/main.f52415c8.chunk.js"
+  },
+  {
+    "revision": "38633e403793b15ed2c8",
+    "url": "/static/js/runtime-main.caab9c03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
