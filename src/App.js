41f56025982ea0f2f96f18847c4bcde0d4b71306
Akibarika <lxclxc89816@gmail.com>
2020-08-07 12:31:26 +1200

    update to react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chart from "react-google-charts";
+
+function App() {
+    return (
+        <Chart
+            width={'900px'}
+            height={'500px'}
+            chartType="Bar"
+            loader={<div>Loading Chart</div>}
+            data={[
+                ['Date', 'Score', 'Number of People'],
+                [new Date(2020, 6, 10), 12, 8], [new Date(2020, 6, 13), 9, 8], [new Date(2020, 6, 14), 12, 8], [new Date(2020, 6, 15), 14, 10], [new Date(2020, 6, 16), 12, 10], [new Date(2020, 6, 17), 11, 11], [new Date(2020, 6, 21), 11, 9], [new Date(2020, 6, 22), 11, 9], [new Date(2020, 6, 23), 14, 10], [new Date(2020, 6, 24), 13, 8], [new Date(2020, 6, 27), 9, 9], [new Date(2020, 6, 28), 12, 10], [new Date(2020, 7, 4), 11, 10], [new Date(2020, 7, 5), 10, 10], [new Date(2020, 7, 6), 8, 11], [new Date(2020, 7, 7), 11, 11]
+            ]}
+            options={{
+                chart: {
+                    title: 'Team Avenger',
+                    subtitle: `Stuff Quiz Score`,
+                }
+            }}
+        />
+    );
+}
+
+export default App;
