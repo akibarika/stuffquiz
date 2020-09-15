9ccbfb9f5a222838c2300642b24139e0f39b5730
Akibarika <lxclxc89816@gmail.com>
2020-09-15 14:23:24 +1200

    Updates

--- precache-manifest.81933ab0edfd15648ec62ab9a2f4e4c1.js
+++ precache-manifest.81933ab0edfd15648ec62ab9a2f4e4c1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4a05fd3d0697fb2c3cf589cd42d88025",
+    "revision": "47282617ed0afc7a7ef6cfee95433ed0",
     "url": "/index.html"
   },
   {
-    "revision": "e71fe8c5134c5773b2a7",
+    "revision": "097b0fe97a4fe9c0ff69",
     "url": "/static/css/main.d1b79232.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bcb38c51.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e71fe8c5134c5773b2a7",
-    "url": "/static/js/main.7318e262.chunk.js"
+    "revision": "097b0fe97a4fe9c0ff69",
+    "url": "/static/js/main.76cfc000.chunk.js"
   },
   {
     "revision": "c66ac07101b2118b8d69",
