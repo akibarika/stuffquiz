e48a4134004a1cabf2346f30fec3538cb66fd7b8
Akibarika <lxclxc89816@gmail.com>
2020-11-20 11:41:43 +1300

    Updates

---
+++ precache-manifest.3dfe6fcf81b09b013e625f84384823a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fc65b68f5e0dc58290cf49d150d19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be3d03adb7169e35af1",
+    "url": "/static/css/main.d1b79232.chunk.css"
+  },
+  {
+    "revision": "da5f28d7494e1f32b2d8",
+    "url": "/static/js/2.bcb38c51.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bcb38c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be3d03adb7169e35af1",
+    "url": "/static/js/main.01ca0dce.chunk.js"
+  },
+  {
+    "revision": "c66ac07101b2118b8d69",
+    "url": "/static/js/runtime-main.8be78b97.js"
+  }
+]);
