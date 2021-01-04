b8ade705350eb98d0ca1b0712ef3e8da78432af8
Akibarika <lxclxc89816@gmail.com>
2021-01-05 10:24:52 +1300

    Updates

---
+++ precache-manifest.c857e00994ceb7b86529b304bf6ebdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb5d6fb4aa85b1717629ae793fec355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb26abcee98bdbeb21c3",
+    "url": "/static/css/main.d1b79232.chunk.css"
+  },
+  {
+    "revision": "da5f28d7494e1f32b2d8",
+    "url": "/static/js/2.bcb38c51.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bcb38c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb26abcee98bdbeb21c3",
+    "url": "/static/js/main.90e9e212.chunk.js"
+  },
+  {
+    "revision": "c66ac07101b2118b8d69",
+    "url": "/static/js/runtime-main.8be78b97.js"
+  }
+]);
